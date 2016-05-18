057fad7488c8c1110be48ea1714ac6acd4c4ca31
JacksonBates <malgalin@gmail.com>
2016-05-18 22:25:02 +1000

    built out api response object

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 var express = require('express')
+var parser = require('ua-parser-js')
 var app = express()
+
 
 app.set('port', (process.env.PORT || 5000))
 
 app.get('/', function(request, response) {
-  response.send('Working')
+var software = parser(request.get('user-agent'))
+var ip = request.ip
+var language = request.get('accept-language')
+var userObject = {
+  "ip-address": ip,
+  "language": language.split(',')[0],
+  "OS": software.os.name + " " + software.os.version
+}
+console.log('software: ' + JSON.stringify(software.os.name) + " " + JSON.stringify(software.os.version))
+console.log('ip: ' + ip)
+console.log('language: ' + language.split(',')[0])
+  response.send(userObject)
 })
 
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), '127.0.0.1', function() {
   console.log('node is running on port 5000...')
 })
